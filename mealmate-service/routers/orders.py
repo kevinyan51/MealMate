e75c6b549f470f0c95e41ceeec1a5dd8f41a28bd
Jamie L <jamie60633@gmail.com>
2023-04-04 15:12:42 -0700

    completed all endpoints in orders

--- mealmate-service/routers/orders.py
+++ mealmate-service/routers/orders.py
@@ -6,7 +6,7 @@ router = APIRouter()
 
 
 @router.get(
-    "/users/{user_id}/orders", response_model=Union[List[OrderOut], Error]
+    "/users/{user_id}/orders/", response_model=Union[List[OrderOut], Error]
 )
 def get_orders_by_subscriber(
     user_id: int,
@@ -14,6 +14,41 @@ def get_orders_by_subscriber(
     repo: OrderRepo = Depends(),
 ):
     result = repo.get_all_by_user(user_id)
+    if result is None:
+        resp.status_code = 500
+    return result
+
+
+@router.get("/orders/", response_model=Union[List[OrderOut], Error])
+def get_all_orders(
+    resp: Response,
+    repo: OrderRepo = Depends(),
+):
+    result = repo.get_all()
+    if result is None:
+        resp.status_code = 500
+    return result
+
+
+@router.post("/orders/", response_model=Union[OrderOut, Error])
+def create_order(
+    box_id: int,
+    resp: Response,
+    repo: OrderRepo = Depends(),
+):
+    result = repo.create(box_id)
+    if result is None:
+        resp.status_code = 500
+    return result
+
+
+@router.get("/orders/{order_id}/", response_model=Union[OrderOut, Error])
+def get_one_order(
+    order_id: int,
+    resp: Response,
+    repo: OrderRepo = Depends(),
+):
+    result = repo.get_one(order_id)
     if result is None:
         resp.status_code = 500
     return result
