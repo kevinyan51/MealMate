ceac8a40488bf39a53ade035afcccdbd6be39499
Kevin Yan <kevinyan51@gmail.com>
2023-04-04 00:37:38 +0000

    put method for chefs's meal

--- mealmate-service/queries/createmeals.py
+++ mealmate-service/queries/createmeals.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 from queries.pool import pool
-
+from typing import Union
 
 class MealIn(BaseModel):
     name: str
@@ -61,3 +61,26 @@ class MealRepository:
                 f"**\nError Message:\n\n {e}\n**"
             )
             return Error(message=str(e))
+
+    def update_meal(self, meal_id: int, meal:MealIn) -> Union[MealOut, Error]:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    db.execute(
+                        '''
+                        UPDATE meals
+                        SET name = %s, name2 = %s, picture_url = %s, description = %s, instructions = %s, ingredients = %s, chef_id = %s, calories = %s, is_keto = %s, is_vegan = %s, is_chef_choice = %s, is_spicy = %s, has_cheese = %s
+                        WHERE id = %s
+                        ''',
+                        [
+                        meal.name, meal.name2, meal.picture_url, meal.description, meal.instructions, meal.ingredients, meal.chef_id, meal.calories, meal.is_keto, meal.is_vegan, meal.is_chef_choice, meal.is_spicy, meal.has_cheese, meal_id
+                        ]
+                    )
+                    return self.meal_in_to_out(meal_id, meal)
+        except Exception as e:
+            print(e)
+            return {"message" : "Could not update meal"}
+
+    def meal_in_to_out(self, id:int, meal:MealIn):
+        old_data = meal.dict()
+        return MealOut(id=id, **old_data)
