d818d168cb8b0c2c426362d56b7291b756963203
Jamie L <jamie60633@gmail.com>
2023-04-05 20:02:09 +0000

    Jamie feat meals

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,14 +1,103 @@
-import { useEffect, useState } from 'react';
+import { createBrowserRouter, Outlet } from 'react-router-dom';
 import './App.css';
-import Box from './Box.js';
+import Nav from './components/Nav.js';
+import MealDetail from './components/MealDetail.js';
+import LandingPage from './pages/LandingPage.js';
+import LoginPage from './pages/LoginPage.js';
+import SignupPage from './pages/SignupPage.js';
+import HomePage from './pages/HomePage.js';
+import ErrorPage from './pages/ErrorPage.js';
+import BoxEditPage from './pages/BoxEditPage.js';
+import MealCreatePage from './pages/MealCreatePage.js';
+import MealEditPage from './pages/MealEditPage.js';
+import OrderListPage from './pages/OrderListPage.js';
+import OrderDetailPage from './pages/OrderDetailPage.js';
 
-function App() {
+const NavbarWrapper = () => {
   return (
-    <div>
-      <h1>Hello world</h1>
-      <Box />
-    </div>
+    <>
+      <Nav />
+      <Outlet />
+      <div style={{ height: 56 }}></div>
+    </>
   );
-}
+};
 
-export default App;
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <NavbarWrapper />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: '', element: <LandingPage /> },
+      { path: 'login', element: <LoginPage /> },
+      { path: 'signup', element: <SignupPage /> },
+      { path: 'home', element: <HomePage /> },
+    ],
+  },
+  {
+    path: '/my-box',
+    element: <NavbarWrapper />,
+    errorElement: <ErrorPage />,
+    children: [{ path: '', element: <BoxEditPage /> }],
+  },
+  {
+    path: '/my-orders',
+    element: <NavbarWrapper />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: '', element: <OrderListPage /> },
+      { path: ':orderId', element: <OrderDetailPage /> },
+    ],
+  },
+  {
+    path: '/meals',
+    element: <NavbarWrapper />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: 'new', element: <MealCreatePage /> },
+      { path: ':mealId', element: <MealDetail /> },
+      { path: ':mealId/edit', element: <MealEditPage /> },
+    ],
+  },
+]);
+
+export default router;
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Nav />
+//       <div style={{ height: 56 }}></div>
+//       <div className="container">
+//         <Routes>
+//           <Route path="/" element={<MainPage />} />
+//           <Route path="/users">
+//             <Route path="login" element={<LoginForm />} />
+//             <Route path="signup" element={<SignupForm />} />
+//           </Route>
+//           <Route path="/meals">
+//             <Route path="" element={<MealList />} />
+//             <Route path="new" element={<MealCreateForm />} />
+//             <Route path=":mealId" element={<MealDetail />} />
+//           </Route>
+//           <Route path="/orders">
+//             <Route path=":mealId" element={<MealDetail />} />
+//             <Route path=":userId" element={<UserOrderDetail />} />
+//           </Route>
+//         </Routes>
+//       </div>
+//       <footer className="fixed-bottom">
+//         <div className="d-flex justify-content-center align-items-center p-2 bg-danger bg-opacity-25 text-muted">
+//           Created with
+//           <i
+//             className="fa fa-heart-o ms-2 me-2"
+//             style={{ fontSize: '1rem', color: 'red' }}
+//           ></i>
+//           by the Culinary Coders. All rights reserved.
+//         </div>
+//       </footer>
+//     </BrowserRouter>
+//   );
+// }
+// export default App;
