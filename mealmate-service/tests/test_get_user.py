d54482ec43908f303904544cdbdc408f9a87f6d0
Caio <caio.chateaubriand.lima@gmail.com>
2023-04-20 11:16:10 -0700

    created a get user test

---
+++ mealmate-service/tests/test_get_user.py
@@ -0,0 +1,26 @@
+from fastapi.testclient import TestClient
+from main import app
+from queries.users import UserQueries
+
+client = TestClient(app)
+
+dummy_user = {
+    "id": 1,
+    "first_name": "Jill",
+    "last_name": "Dill",
+    "username": "jilldill",
+    "email": "jd@mm.com",
+    "hashed_password": "strongestpassword",
+    "picture_url": "",
+    "role_id": 1
+}
+
+
+class MockUserQuerie:
+    def get_user(self, username):
+        return [dummy_user] if username == dummy_user["username"] else None
+def test_get_user():
+    app.dependency_overrides[UserQueries] = MockUserQuerie
+    response = client.get("/testing")
+    assert response.status_code == 200
+    app.dependency_overrides = {}
