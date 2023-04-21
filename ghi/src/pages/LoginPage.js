98d378628272d6d836ebbd96b5f331734b0dae79
Caio <caio.chateaubriand.lima@gmail.com>
2023-04-20 18:29:05 -0700

    unit test working and still debugging fe

--- ghi/src/pages/LoginPage.js
+++ ghi/src/pages/LoginPage.js
@@ -1,7 +1,48 @@
-import React from 'react';
+import { useState } from "react";
+import { loginUser } from "./Auth";
 
-const LoginPage = () => {
-  return <div>LoginPage</div>;
-};
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await loginUser({ username, password });
+      localStorage.setItem("token", response.token);
+      setErrorMessage("");
+    } catch (error) {
+      setErrorMessage("Cannot login");
+    }
+  };
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      // redirect to home page
+      window.location.href = "/";
+    }
+  }, []);
+
+  return (
+    <div>
+      <form onSubmit={handleLogin}>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+      {errorMessage && <p>{errorMessage}</p>}
+    </div>
+  );
+}
 
 export default LoginPage;
--- ghi/src/pages/SignupPage.js
+++ ghi/src/pages/SignupPage.js
@@ -1,7 +1,156 @@
-import React from 'react';
+import React, { useState } from "react";
 
-const SignupPage = () => {
-  return <div>SignupPage</div>;
-};
+function SignupPage() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [pictureUrl, setPictureUrl] = useState("");
+  const [roleId, setRoleId] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = {};
+    data.first_name = firstName;
+    data.last_name = lastName;
+    data.username = username;
+    data.email = email;
+    data.password = password;
+    data.picture_url = pictureUrl;
+    data.role_id = roleId;
+
+    const userUrl = `${process.env.REACT_APP_MEALMATE_SERVICE_API_HOST}/users`;
+    const fetchConfig = {
+      method: "post",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const response = await fetch(userUrl, fetchConfig);
+    if (response.ok) {
+      setFirstName("");
+      setLastName("");
+      setUsername("");
+      setEmail("");
+      setPassword("");
+      setPictureUrl("");
+      setRoleId("");
+    }
+  };
+
+  return (
+    <div className="row">
+      <div className="offset-3 col-6">
+        <div className="shadow p-4 mt-4">
+          <h1>Signup</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="form-floating mb-3">
+              <input
+                onChange={(e) => setFirstName(e.target.value)}
+                value={firstName}
+                placeholder="First Name"
+                required
+                type="text"
+                name="firstName"
+                id="firstName"
+                className="form-control"
+              />
+              <label htmlFor="firstName">First Name</label>
+            </div>
+
+            <div className="form-floating mb-3">
+              <input
+                onChange={(e) => setLastName(e.target.value)}
+                value={lastName}
+                placeholder="Last Name"
+                required
+                type="text"
+                name="lastName"
+                id="lastName"
+                className="form-control"
+              />
+              <label htmlFor="lastName">Last Name</label>
+            </div>
+
+            <div className="form-floating mb-3">
+              <input
+                onChange={(e) => setUsername(e.target.value)}
+                value={username}
+                placeholder="Username"
+                required
+                type="text"
+                name="username"
+                id="username"
+                className="form-control"
+              />
+              <label htmlFor="username">Username</label>
+            </div>
+
+            <div className="form-floating mb-3">
+              <input
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                placeholder="Email"
+                required
+                type="text"
+                name="email"
+                id="email"
+                className="form-control"
+              />
+              <label htmlFor="email">Email</label>
+            </div>
+
+            <div className="form-floating mb-3">
+              <input
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                placeholder="Password"
+                required
+                type="text"
+                name="password"
+                id="password"
+                className="form-control"
+              />
+              <label htmlFor="password">Password</label>
+            </div>
+
+            <div className="form-floating mb-3">
+              <input
+                onChange={(e) => setPictureUrl(e.target.value)}
+                value={pictureUrl}
+                placeholder="Picture Url"
+                required
+                type="text"
+                name="pictureUrl"
+                id="pictureUrl"
+                className="form-control"
+              />
+              <label htmlFor="pictureUrl">Picture URL</label>
+            </div>
+
+            <div className="form-floating mb-3">
+              <input
+                onChange={(e) => setRoleId(e.target.value)}
+                value={roleId}
+                placeholder="Role Id"
+                required
+                type="int"
+                name="Role Id"
+                id="roleId"
+                className="form-control"
+              />
+              <label htmlFor="roleId">Role ID</label>
+            </div>
+
+            <button className="btn btn-primary">Create</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default SignupPage;
