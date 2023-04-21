50868e97c44881031c9b9868a0a3d1e0fac35739
Mason McInerny <masonmcinerny@gmail.com>
2023-04-20 18:52:45 -0700

    push before grabbing ciao push

--- mealmate-service/tests/test_single_meal.py
+++ mealmate-service/tests/test_single_meal.py
@@ -7,13 +7,12 @@ client = TestClient(app)
 
 
 test_meal = {
-    "meal_id": 1,
-    "status_id": 1,
+    "id": 1,
     "chef_id": 1,
     "name": "Test Meal",
     "name2": "This is a test meal",
-    "created_at":"2021-01-01T00:00:00",
-    "updated_at": "2021-01-01T00:00:00",
+    "created_at":"2021-01-01",
+    "updated_at": "2021-01-01",
     "picture_url": "thisisatestpictureurl.com",
     "description": "this meal was designed to test",
     "instructions": "test the instructions",
@@ -25,27 +24,24 @@ test_meal = {
     "is_spicy": True,
     "has_cheese": True,
     "price": 1.00,
-    "chef_first_name": "Guy",
-    "chef_last_name": "fieri",
+    "first_name": "Guy",
+    "last_name": "fieri",
     "chef_picture_url": "insertcoolpicofguyfieri.here",
 }
 
 class SingleMealRepo:
-    def get_one(self):
-        return[
-            {
-            "meal_id": 1,
+    def get_one(self, id:int):
+        return{
+            "id": 1,
             **test_meal,
             }
-        ]
+
 
 def test_get_single_meal():
-    app.dependency_override[MealDetailsRepository] = SingleMealRepo
+    app.dependency_overrides[MealDetailsRepository] = SingleMealRepo
     response = client.get("/api/meals/1/")
     app.dependecy_overrides = {}
     assert response.status_code == 200
-    assert response.json() == [
-        {"meal_id": 1,
+    assert response.json() == {"id": 1,
          **test_meal,
         }
-    ]
