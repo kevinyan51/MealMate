1765976dda1ef4e910e840cad2cd8b9d83ab97e3
Jamie L <jamie60633@gmail.com>
2023-04-24 18:08:11 -0700

    remove unused import

--- ghi/src/components/Auth.js
+++ ghi/src/components/Auth.js
@@ -1,6 +1,5 @@
 import { createContext, useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import jwt_decode from 'jwt-decode';
 
 let internalToken = null;
 
