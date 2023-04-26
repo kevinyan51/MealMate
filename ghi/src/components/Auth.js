8ca2b0d8e8c4a36429b6d5547562ff221d207a7a
Caio <caio.chateaubriand.lima@gmail.com>
2023-04-26 11:45:16 -0700

    deploy test

--- ghi/src/components/Auth.js
+++ ghi/src/components/Auth.js
@@ -1,5 +1,5 @@
-import { createContext, useContext, useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { createContext, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 let internalToken = null;
 
@@ -11,34 +11,34 @@ export async function getTokenInternal() {
   const url = `${process.env.REACT_APP_USER_SERVICE_API_HOST}/api/token/`;
   try {
     const response = await fetch(url, {
-      credentials: 'include',
+      credentials: "include",
     });
     if (response.ok) {
       const data = await response.json();
       return { account: data?.account, access_token: data?.access_token };
     }
   } catch (e) {
-    console.error('error getting token', e);
+    console.error("error getting token", e);
   }
   return { account: null, access_token: null };
 }
 
 function handleErrorMessage(error) {
-  if ('error' in error) {
+  if ("error" in error) {
     error = error.error;
     try {
       error = JSON.parse(error);
-      if ('__all__' in error) {
+      if ("__all__" in error) {
         error = error.__all__;
       }
     } catch {}
   }
   if (Array.isArray(error)) {
-    error = error.join('<br>');
-  } else if (typeof error === 'object') {
+    error = error.join("<br>");
+  } else if (typeof error === "object") {
     error = Object.entries(error).reduce(
       (acc, x) => `${acc}<br>${x[0]}: ${x[1]}`,
-      ''
+      ""
     );
   }
   return error;
@@ -80,22 +80,22 @@ export function useToken() {
   async function logout() {
     if (token) {
       const url = `${process.env.REACT_APP_USER_SERVICE_API_HOST}/api/token`;
-      await fetch(url, { method: 'delete', credentials: 'include' });
+      await fetch(url, { method: "delete", credentials: "include" });
       internalToken = null;
       setToken(null);
       setUser(null);
-      navigate('/');
+      navigate("/");
     }
   }
 
   async function login(username, password) {
     const url = `${process.env.REACT_APP_USER_SERVICE_API_HOST}/api/token`;
     const form = new FormData();
-    form.append('username', username);
-    form.append('password', password);
+    form.append("username", username);
+    form.append("password", password);
     const response = await fetch(url, {
-      method: 'post',
-      credentials: 'include',
+      method: "post",
+      credentials: "include",
       body: form,
     });
     if (response.ok) {
@@ -117,9 +117,9 @@ export function useToken() {
     pictureUrl,
     roleId,
   }) {
-    const url = `${process.env.REACT_APP_USER_SERVICE_API_HOST}/api/users`;
+    const url = `https://mealmate-api.dec-pt-15.mod3projects.com/api/users`;
     const response = await fetch(url, {
-      method: 'post',
+      method: "post",
       body: JSON.stringify({
         first_name: firstName,
         last_name: lastName,
@@ -130,12 +130,12 @@ export function useToken() {
         role_id: roleId,
       }),
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     });
     if (response.ok) {
       await login(username, password);
-      console.log('Signup Successful');
+      console.log("Signup Successful");
     } else if (response.status >= 400 && response.status < 600) {
       const body = await response.json();
       throw Error(body.detail);
@@ -154,7 +154,7 @@ export function useToken() {
   ) {
     const url = `${process.env.REACT_APP_USER_SERVICE_API_HOST}/api/users/`;
     const response = await fetch(url, {
-      method: 'put',
+      method: "put",
       body: JSON.stringify({
         first_name: firstName,
         last_name: lastName,
@@ -164,7 +164,7 @@ export function useToken() {
         picture_url: pictureUrl,
       }),
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     });
     if (response.ok) {
@@ -187,7 +187,7 @@ export const useUser = (token) => {
     async function getUser() {
       const url = `${process.env.REACT_APP_USER_SERVICE_API_HOST}/api/users/`;
       const response = await fetch(url, {
-        credentials: 'include',
+        credentials: "include",
       });
       if (response.ok) {
         const newUser = await response.json();
