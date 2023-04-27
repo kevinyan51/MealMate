20e48bbd63f00d8d1de9ac5af884fd844382a293
Jamie L <jamie60633@gmail.com>
2023-04-26 21:58:11 -0700

    user profile

--- ghi/src/pages/UserProfilePage.js
+++ ghi/src/pages/UserProfilePage.js
@@ -1,18 +1,157 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useToken } from '../components/Auth';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const UserProfilePage = () => {
   const { user } = useToken();
+
+  const [selectedAvatar, setSelectedAvatar] = useState(user?.picture_url);
+  const [firstName, setFirstName] = useState(user?.first_name);
+  const [lastName, setLastName] = useState(user?.last_name);
+  const [email, setEmail] = useState(user?.email);
+  const [emailValid, setEmailValid] = useState(true);
+  const [success, setSuccess] = useState(false);
+
+  const avatarOptions = [
+    'https://res.cloudinary.com/dfdnr2jby/image/upload/v1682569873/Boy_headphones_lti3xz.png',
+    'https://res.cloudinary.com/dfdnr2jby/image/upload/v1682569873/Woman_j4rs6u.png',
+    'https://res.cloudinary.com/dfdnr2jby/image/upload/v1682569873/Woman_muslim_jf0peb.png',
+    'https://res.cloudinary.com/dfdnr2jby/image/upload/v1682569873/Astronaut_dlwnxg.png',
+    'https://res.cloudinary.com/dfdnr2jby/image/upload/v1682569873/Afroamerican_lb2kxl.png',
+    'https://res.cloudinary.com/dfdnr2jby/image/upload/v1682569873/Scientist_kqoouj.png',
+    'https://res.cloudinary.com/dfdnr2jby/image/upload/v1682569873/Girl_zj4ylc.png',
+  ];
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+    setEmailValid(event.target.checkValidity());
+  };
+
+  const handleFirstNameChange = (event) => {
+    setFirstName(event.target.value);
+  };
+
+  const handleLastNameChange = (event) => {
+    setLastName(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Handle form submission here
+    setSuccess(true);
+  };
+
   return (
-    <div>
-      <h1>User Profile Page</h1>
-      <div>userid: {user?.id}</div>
-      <div>username: {user?.username}</div>
-      <div>user firstname: {user?.firstname}</div>
-      <div>user lastname: {user?.last_name}</div>
-      <div>user email: {user?.email}</div>
-      <div>user picture: {user?.picture_url}</div>
-    </div>
+    <Container>
+      <Row className="justify-content-center mt-5">
+        <Col md={8}>
+          <h1 className="text-center mb-4">User Profile Page</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+              <label htmlFor="userId" className="form-label">
+                User ID:
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="userId"
+                defaultValue={user?.id}
+                // readOnly
+                disabled
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="username" className="form-label">
+                Username:
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="username"
+                defaultValue={user?.username}
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="firstName" className="form-label">
+                User First Name:
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="firstName"
+                defaultValue={user?.first_name}
+                onChange={handleFirstNameChange}
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="lastName" className="form-label">
+                User Last Name:
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="lastName"
+                defaultValue={user?.last_name}
+                onChange={handleLastNameChange}
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
+                User Email:
+              </label>
+              <input
+                type="email"
+                className={`form-control ${emailValid ? '' : 'is-invalid'}`}
+                id="email"
+                defaultValue={user?.email}
+                onChange={handleEmailChange}
+                required
+              />
+              <div className="invalid-feedback">
+                Please enter a valid email address
+              </div>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="pictureUrl" className="form-label">
+                Select Avatar:
+              </label>
+              <div className="d-flex flex-row">
+                {avatarOptions.map((url) => (
+                  <img
+                    key={url}
+                    src={url}
+                    alt="avatar option"
+                    height="50"
+                    width="50"
+                    className={`ms-2 ${
+                      url === selectedAvatar ? 'border border-primary' : ''
+                    }`}
+                    onClick={() => setSelectedAvatar(url)}
+                  />
+                ))}
+              </div>
+            </div>
+            <div className="d-grid">
+              <button
+                type="submit"
+                className="btn btn-success"
+                disabled={!emailValid}
+              >
+                Update Profile
+              </button>
+            </div>
+          </form>
+          {success && (
+            <div className="alert alert-success mt-3" role="alert">
+              Your profile has been updated!
+            </div>
+          )}
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
